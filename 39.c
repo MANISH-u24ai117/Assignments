47cabbe944fe334fd57d517c382bee5a9e030e03
MANISH-u24ai117 <u24ai117@aid.svnit.ac.in>
2024-11-25 18:49:19 +0530

    Add files via upload

---
+++ 39.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    printf("Enter the no of terms :- ");
+        int n;
+        scanf("%d", &n);
+        for(int i=1; i<=n; i++ ){
+    
+            printf("%d/%d!+", i,i);
+        }
+    
+}
